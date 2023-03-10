678c0c8b0726fd218138d168d1a83fad84ca7ac5
JJinBBangg <aingdevil10@naver.com>
2023-03-10 13:47:18 +0900

    java

--- JAVA/java20230214/src/ex/thisisjava/exam1304/UtilExample.java
+++ JAVA/java20230214/src/ex/thisisjava/exam1304/UtilExample.java
@@ -1,17 +1,13 @@
 package ex.thisisjava.exam1304;
 
 public class UtilExample {
-//	public static void main(String[] args) {
-//		Pair<String, Integer> pair = new Pair<>("홍길동", 35);
-//		Integer age = Util.getValue(pair, "홍길동");
-//		System.out.println(age);
-//		
-//		ChildPair<String, Integer> childPair = new ChildPair<>("홍상원", 20);
-//		Integer childAge = Util.getValue(childPair, "홍삼순");
-//		System.out.println(childAge);
-//	}
-}
-class Pair<T1, T2> {
-}
-class ChildPair<T1, T2> {
+	public static void main(String[] args) {
+		Pair<String, Integer> pair = new Pair<>("홍길동", 35);
+		Integer age = Util.getValue(pair, "홍길동");
+		System.out.println(age);
+		
+		ChildPair<String, Integer> childPair = new ChildPair<>("홍상원", 20);
+		Integer childAge = Util.getValue(childPair, "홍삼순");
+		System.out.println(childAge);
+	}
 }
