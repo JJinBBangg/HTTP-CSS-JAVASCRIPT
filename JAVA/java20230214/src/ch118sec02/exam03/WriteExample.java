94ec6e3da9de4ee79220c48e15eeaefd0a54b9ef
JJinBBangg <123609823+JJinBBangg@users.noreply.github.com>
2023-03-22 16:22:28 +0900

    java

--- JAVA/java20230214/src/ch118sec02/exam03/WriteExample.java
+++ JAVA/java20230214/src/ch118sec02/exam03/WriteExample.java
@@ -1,12 +1,12 @@
-package ch118sec02.exam02;
+package ch118sec02.exam03;
 
 import java.io.*;
 
 public class WriteExample {
 	public static void main(String[] args) {
-		try (OutputStream os = new FileOutputStream("output/test2.db")) {
-			byte[] array= { 10, 20, 30};
-			os.write(array);
+		try (OutputStream os = new FileOutputStream("output/test3.db")) {
+			byte[] array = { 10, 20, 30, 40, 50 };
+			os.write(array, 1, 3);
 			os.flush();
 			os.close();
 		} catch (IOException e) {
--- JAVA/java20230214/src/ex/programmers/Ex120813.java
+++ JAVA/java20230214/src/ex/programmers/Ex120813.java
@@ -6,14 +6,15 @@ public class Ex120813 {
 	public static void main(String[] args) {
 		int n = 5;
 		List<Integer> list = new ArrayList<>();
-		for (int i = n; i > 0; i--) {
+		for (int i = 1; i <= n; i++) {
 			if (i % 2 != 0) {
 				list.add(i);
 			}
 		}
-		List<String> arr = List.of("Ad", "asd", "Asd");
-		String[] answer1 = arr.toArray(new String[arr.size()]);
+		int[] answer = new int[list.size()];
+		for(int i = 0; i < list.size(); i++) {
+			answer[i]=list.get(i);
+		}
 		
-//		int[] answer = list.toArray(new Integer[list.size()]);
 	}
 }
