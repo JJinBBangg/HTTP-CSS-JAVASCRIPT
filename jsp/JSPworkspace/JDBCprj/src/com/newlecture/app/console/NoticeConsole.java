3a113e14d0e110495cf9d6aa82eee89d14e50f2b
JJinBBangg <123609823+JJinBBangg@users.noreply.github.com>
2023-03-23 09:11:03 +0900

    jsp

---
+++ jsp/JSPworkspace/JDBCprj/src/com/newlecture/app/console/NoticeConsole.java
@@ -0,0 +1,76 @@
+package com.newlecture.app.console;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+import com.newlecture.app.entity.Notice;
+import com.newlecture.app.service.NoticeService;
+
+public class NoticeConsole {
+
+	private NoticeService service;
+	private int page;
+	private int count;
+	private String searchField;
+	private String searchWord;
+
+	public NoticeConsole() {
+		service = new NoticeService();
+		page = 1;
+		count = 0;
+		searchField = "TITLE";
+		searchWord = "";
+	}
+
+	public void printNoticeList() throws ClassNotFoundException, SQLException {
+		List<Notice> list = service.getList(page, searchField, searchWord);
+		count = service.getCount();
+		System.out.println("-----------------------------------------------");
+		System.out.printf("<공지사항> 총 %d 게시글\n", count);
+		System.out.println("-----------------------------------------------");
+		for (Notice n : list) {
+			System.out.printf("%d. %s / %s  / %s\n",
+					n.getId(), n.getTitle(), n.getWriterId(), n.getRegDate());
+		}
+		System.out.println("-----------------------------------------------");
+		System.out.printf("             %d/%d pages\n", page, (int)Math.ceil(count/10.0));
+		System.out.println("-----------------------------------------------");
+	}
+
+	public int inputNoticeMenu() {
+		Scanner scanner = new Scanner(System.in);
+		// 상세조회
+		System.out.printf("1.상세조회 / 2.이전/ 3.다음/ 4.글쓰기/ 5.검색/ 6.종료 >");
+		int menu_ = Integer.valueOf(scanner.nextLine());
+
+		return menu_;
+	}
+
+	public void movePrevList() {
+		if (page == 1) {
+			System.out.println("이전페이지가 없습니다.");
+			return;
+		}
+		page--;
+
+	}
+
+	public void moveNextList() throws ClassNotFoundException, SQLException {
+		count = service.getCount();
+		if(page ==  (int)Math.ceil(count/10.0)) {
+		System.out.println("마지막페이지입니다.");
+		return;
+	}
+		page++;
+	}
+
+	public void inputSearchWord() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("검색 범주(title/content/writerId)중에 하나를 입력하세요");
+		System.out.println(">");
+		searchField =scanner.nextLine();
+		System.out.println("검색어>");
+		searchWord = scanner.nextLine();
+	}
+}
--- jsp/JSPworkspace/JDBCprj/src/com/newlecture/app/service/NoticeService.java
+++ jsp/JSPworkspace/JDBCprj/src/com/newlecture/app/service/NoticeService.java
@@ -3,7 +3,9 @@ package com.newlecture.app.service;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,41 +13,145 @@ import java.util.List;
 import com.newlecture.app.entity.Notice;
 
 public class NoticeService {
-	public List<Notice> getList(){
-		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String sql = "SELECT * FROM NEWLEC.NOTICE";
-		
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection(url, "NEWLEC", "1234");
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(sql);
-		
+	private String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+	private String uid = "NEWLEC";
+	private String pwd = "1234";
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+
+	public List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException {
+
+		int start = 1 + (page - 1) * 10;
+		int end = 10 * page;
+
+		String sql = "select * from notice_view WHERE "+field+" LIKE ? AND NUM BETWEEN ? AND ?";
+
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, uid, pwd);
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, "%"+query+"%");
+		st.setInt(2, start);
+		st.setInt(3, end);
+		ResultSet rs = st.executeQuery();
+
 		List<Notice> list = new ArrayList<>();
-		
+
 		while (rs.next()) {
-			int id = rs.getInt("id");
-			String title = rs.getString("title");
-			String writerId = rs.getString("writer_id");
-			Date regDate = rs.getDate("regdate");
-			String content = rs.getString("content");
+			int id = rs.getInt("ID");
+			String title = rs.getString("TITLE");
+			String writerId = rs.getString("WRITER_ID");
+			Date regDate = rs.getDate("REGDATE");
+			String content = rs.getString("CONTENT");
 			int hit = rs.getInt("hit");
-			
+			String files = rs.getString("FILES");
+
 			Notice notice = new Notice(
 					id,
 					title,
 					writerId,
-					regDate
-					Content,
-					hit
-					);
-			System.out.printf("id:%d, title:%s, writer_id:%s, regDate:%s, content:%s, hit:%d \n",
-					id, title, writerId, regDate, content, hit);
+					regDate,
+					content,
+					hit,
+					files);
+			list.add(notice);
 		}
+		rs.close();
+		st.close();
+		con.close();
+
+		return list;
+
+	}
+
+	// Scalar 값
+	public int getCount() throws ClassNotFoundException, SQLException {
+		int count = 0;
+		String sql = "select COUNT(ID) COUNT from notice";
+
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, uid, pwd);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		if (rs.next())
+			count = rs.getInt("COUNT");
 
 		rs.close();
 		st.close();
 		con.close();
-		
-		return ?;
+
+		return count;
 	}
+
+	public int insert(Notice notice) throws ClassNotFoundException, SQLException {
+		String title = notice.getTitle();
+		String writerId = notice.getWriterId();
+		String content = notice.getContent();
+		String files = notice.getFiles();
+
+		String sql = "INSERT INTO notice ("
+				+ "    title,"
+				+ "    writer_id,"
+				+ "    content,"
+				+ "    files"
+				+ ") VALUES (?,?,?,?)";
+
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, uid, pwd);
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, title);
+		st.setString(2, writerId);
+		st.setString(3, content);
+		st.setString(4, files);
+
+		int result = st.executeUpdate();
+
+		st.close();
+		con.close();
+		return result;
+	}
+
+	public int update(Notice notice) throws ClassNotFoundException, SQLException {
+		String title = notice.getTitle();
+		;
+		String content = notice.getContent();
+		String files = notice.getFiles();
+		int id = notice.getId();
+
+		String sql = "UPDATE notice "
+				+ "SET"
+				+ "    title = ?,"
+				+ "    content = ?,"
+				+ "    files = ?"
+				+ "    where id = ?";
+
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, uid, pwd);
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, title);
+		st.setString(2, content);
+		st.setString(3, files);
+		st.setInt(4, id);
+
+		int result = st.executeUpdate();
+
+		st.close();
+		con.close();
+		return result;
+	}
+
+	public int delete(int id) throws ClassNotFoundException, SQLException {
+
+		String sql = "DELETE notice where id = ?";
+
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, uid, pwd);
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, id);
+
+		int result = st.executeUpdate();
+
+		st.close();
+		con.close();
+		return result;
+	}
+
 }
